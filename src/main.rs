44fa14c78ccdbbaf8faa880c04ef4a082a02e772
Kyle Gretchev <kyle.gretchev@magnetforensics.com>
2019-05-03 14:57:00 -0400

    Updated deps

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use combine::{
     char::{self, space},
     many, many1,
     range::{range, take_until_range, take_while, take_while1},
-    skip_many, skip_many1,
+    satisfy, skip_many, skip_many1,
     stream::state::State,
-    ParseError, Parser, RangeStream,
+    token, ParseError, Parser, RangeStream,
 };
 use std::{fs, path::PathBuf};
 use structopt::StructOpt;
@@ -22,7 +22,10 @@ struct Opts {
 fn main() {
     let opt = Opts::from_args();
     let data = fs::read_to_string(opt.input).expect("Could not read input file");
-    let res = section().easy_parse(State::new(data.as_str()));
+    let res = valid_line()
+        .with(host())
+        .skip(maybe_whitespace())
+        .easy_parse(State::new(data.as_str()));
 
     dbg!(res);
 }
@@ -35,7 +38,29 @@ where
     skip_many1(space())
 }
 
-fn host_name<'a, I>() -> impl Parser<Input = I, Output = &'a str>
+fn maybe_whitespace<'a, I>() -> impl Parser<Input = I>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    skip_many(space())
+}
+
+fn valid_line<'a, I>() -> impl Parser<Input = I, Output = (&'a str, &'a str)>
+where
+    I: RangeStream<Item = char, Range = &'a str>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    (
+        take_while(|c: char| !c.is_whitespace()),
+        whitespace(),
+        take_while1(|c: char| !c.is_whitespace()),
+    )
+        .map(|(key, _, value)| (key, value))
+        .message("while parsing host name")
+}
+
+fn host<'a, I>() -> impl Parser<Input = I, Output = (&'a str, &'a str)>
 where
     I: RangeStream<Item = char, Range = &'a str>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
@@ -45,40 +70,8 @@ where
         whitespace(),
         take_while1(|c: char| !c.is_whitespace()),
     )
-        .map(|(_, _, h)| h)
+        .map(|(_, _, value)| ("SHEEE EIT", value))
         .message("while parsing host name")
-}
-
-fn property<'a, I>() -> impl Parser<Input = I, Output = (&'a str, &'a str)>
-where
-    I: RangeStream<Item = char, Range = &'a str>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    (
-        take_while(|c: char| !c.is_whitespace()), // This needs to take NOT 'Host'
-        whitespace(),
-        take_while(|c: char| !c.is_whitespace()),
-    )
-        .map(|(key, _, value)| (key, value))
-        .message("while parsing property")
-}
-
-fn properties<'a, I>() -> impl Parser<Input = I, Output = Vec<(&'a str, &'a str)>>
-where
-    I: RangeStream<Item = char, Range = &'a str>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    many(property().skip(whitespace())).message("while parsing properties")
-}
-
-fn section<'a, I>() -> impl Parser<Input = I, Output = (&'a str, Vec<(&'a str, &'a str)>)>
-where
-    I: RangeStream<Item = char, Range = &'a str>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    (host_name(), whitespace(), properties())
-        .map(|(host_name, _, properties)| (host_name, properties))
-        .message("while parsing hosts")
 }
 
 #[cfg(test)]
